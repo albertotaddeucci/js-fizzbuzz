28974dfe16c7d554f148f573303e55716c5b0d7e
alberto <alberto.taddeucci@gmail.com>
2024-01-31 14:14:33 +0100

    visualizzazione script in pagina

--- js/script.js
+++ js/script.js
@@ -4,23 +4,36 @@ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipl
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
 
+const grigliaElement = document.querySelector("#griglia");
+
+
 for (let i=1; i<=100; i++){
+
+    const newElement = document.createElement("div");
+    // console.log(newElement)
+    
+    grigliaElement.append(newElement);
     
     //divisibile per entrambi
     if (i % 3 == 0 && i % 5 == 0){
-        console.log("fizzbuzz")
+        // console.log("fizzbuzz")
+        newElement.innerHTML = "fizzbuz";
 
     //divisibile per 3
     } else if (i % 3 == 0){
-        console.log("fizz")
+        // console.log("fizz")
+        newElement.innerHTML = "fizz";
+
     
     //divisibile per 5
     } else if (i % 5 == 0) {
-        console.log("buzz")
+        // console.log("buzz")
+        newElement.innerHTML = "buzz";
 
        
     } else {
-        console.log(i)
+        newElement.innerHTML = `${i}`;
+
     }
 
 }
